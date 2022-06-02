ea70dbc6a9d05449f0e96aea108ecfbadea8c836
chauid <dlfdl28@naver.com>
2022-06-02 19:17:07 +0900

    Upload webpage 0.1ver

--- script/Master.js
+++ script/Master.js
@@ -1,8 +1,10 @@
 function buttonclick() {
     var buttonbox = document.getElementById("Menu");
     var sidemenu = document.getElementById("Nav");
+    var sideinner = document.getElementById("Nav_inner");
     var header = document.getElementById("Header");
     buttonbox.classList.toggle('-active');
     sidemenu.classList.toggle('-active');
+    sideinner.classList.toggle('-active');
     header.classList.toggle('-active');
 }
